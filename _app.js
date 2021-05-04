408d79e4844863bd2674ed440b15272ceb005a06
manhcuong1524 <81214314+manhcuong1524@users.noreply.github.com>
2021-05-04 10:34:37 +0700

    Add files via upload

---
+++ _app.js
@@ -0,0 +1,10 @@
+import '../styles/globals.css'
+import '../styles/contact.scss';
+import '../styles/footer.scss';
+import '../styles/aboutus.scss';
+
+function MyApp({ Component, pageProps }) {
+  return <Component {...pageProps} />
+}
+
+export default MyApp
---
+++ about.js
@@ -0,0 +1,39 @@
+import React, { Component } from 'react';
+
+class about extends Component {
+    render() {
+        return (
+            <div>
+                <div className="p1" style={{ background: 'url("https://cdn.shopify.com/s/files/1/0552/1735/6962/files/D686l_YWkAQcBij.png?v=1617141486") 50% 50% no-repeat', backgroundSize: 'cover' }}>
+                    
+    <div className="rgbablackstrong"> 
+                        <p className="p11"><span>ABOUT</span> <br /><br />Knight Owl Labs is a California-based manufacturer of PC custom
+        components that help redesign the generic look of computers and to create something much more unique and
+        personalized. Our team is a collection of young and aspiring individuals, taking on a new twist with the
+        idea of capturing and inspiring others' passions through gaming and art.</p>
+                    </div>
+                </div>
+                <p className="p2" style={{ background: 'url("https://cdn.shopify.com/s/files/1/0552/1735/6962/files/gpu-backplate-featured-image.jpg?v=1616526751") 50% 50% no-repeat', backgroundSize: 'cover' }}>
+                    <span>OUR MISSION</span><br /><br />To provide a platform for PC enthusiast to expand their creativity and go
+    beyond. Knight Owl's mission is to create customizable PC accessories that customers can enjoy and take pride in.
+    These are the core-values that motivate and guide us in bringing you the finest PC gaming products possible.</p>
+                <p className="p33" style={{ background: 'url("https://cdn.originpc.com/img/company/about/about-bg.jpg")50% 50% no-repeat', backgroundSize: 'cover' }}>
+                    <span>PRODUCTION INSURANCE &amp; INTEGRITY</span><br /><br />Perfection and accuracy is what we aim for. By
+    implementing a rigorous quality-control process, we ensure to fulfill and exceed customer's expectation based on how
+    the product looks and its overall functionality.</p>
+                <p id="legacy" style={{ background: 'url("https://cdn.originpc.com/img/company/about/legacy-bg.jpg") 50% 50% no-repeat', backgroundSize: 'cover' }}>
+                    <span>AFFORDABLE PRICE</span><br /><br />We believe that everyone should have easy access to quality PC accessories
+    and enjoy the advantage technology offers. We are working tirelessly with multiple suppliers to ensure that the
+    community will receive the highest value products.</p>
+                <p className="p33" style={{ background: 'url("https://cdn.originpc.com/img/company/about/about-bg.jpg") 50% 50% no-repeat', backgroundSize: 'cover' }}>
+                    <span>CUSTOMER SATISFACTION</span><br /><br />We respect and value our customer to the fullest. We want to provide
+    and show a deep appreciation for our members through honest and helpful customer service both before and after their
+    purchases with Knight Owl Labs. Our 24/7 Customer Service team are ready to assist you through any questions ranging
+    from product information, quote, order status, etc.</p>
+            </div>
+
+        );
+    }
+}
+
+export default about;
